cf8861fd84356015f2804d37c9b5254b4a48ce84
Archerprop <archerprop@outlook.com>
2022-10-31 20:26:53 -0500

    actualizo archivos programa

---
+++ src/main/java/com/archerprop/appinventorypc/AppInventoryPC.java
@@ -0,0 +1,23 @@
+package com.archerprop.appinventorypc;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aschente
+ */
+@SpringBootApplication
+@Log4j2
+public class AppInventoryPC {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppInventoryPC.class, args);
+    }
+}
